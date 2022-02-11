5f40e529970746de1d39de376240484baacbc8b1
mikeyj913 <mikeyj913@hotmail.com>
2022-02-11 11:53:02 -0500

    Base layout

---
+++ main_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+# this like app.use() in express
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('main_app.urls')),
+     path('', views.Home.as_view(), name="home"),
+]
