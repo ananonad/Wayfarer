7daac8f99f69881a7bb19d91060ca4a211e43a81
mikeyj913 <mikeyj913@hotmail.com>
2022-02-11 13:45:56 -0500

    about and index html connected and path added

--- main_app/views.py
+++ main_app/views.py
@@ -10,3 +10,23 @@ class Home(View):
 
 # Create your views here.
 
+class About(View):
+
+    def get(self, request):
+        return HttpResponse("Wayfarer About")
+
+class Index(View):
+
+    def get(self, request):
+        return HttpResponse("Wayfarer Index")
+        
+class Home(TemplateView):
+    template_name = "home.html"
+
+#...
+class About(TemplateView):
+    template_name = "about.html"
+
+#...
+class Index(TemplateView):
+    template_name = "index.html"
