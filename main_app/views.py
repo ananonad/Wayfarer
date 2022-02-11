a298a9e107a7e3fe9721e21fc66fec8b2972e93d
mariogerardi <mgerardi@vassar.edu>
2022-02-11 14:00:43 -0500

    Added TemplateView.

--- main_app/views.py
+++ main_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Place
 from django.views import View 
 from django.http import HttpResponse
+from django.views.generic.base import TemplateView
 
 class Home(View):
     def get(self, request):
