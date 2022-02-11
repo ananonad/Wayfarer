c892c05fc3c4675c6026290b9fdb601db68c73a6
annanadd <ananonadd@gmail.com>
2022-02-11 16:52:44 -0500

    sql

--- main_app/views.py
+++ main_app/views.py
@@ -7,11 +7,11 @@ from django.views.generic.base import TemplateView
 class Home(TemplateView):
     template_name = "home.html"
 
-#...
+
 class About(TemplateView):
     template_name = "about.html"
 
-#...
+
 class Index(TemplateView):
     template_name = "index.html"
 
