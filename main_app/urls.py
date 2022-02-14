933ee098ec8a578dc1429ee6b60ae477b00e731a
mariogerardi <mgerardi@vassar.edu>
2022-02-14 17:38:36 -0500

    Changed a couple names here and there?

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,14 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-     path('', views.Home.as_view(), name="home"),
+     path('', views.Home.as_view(), name="spacefarer"),
      path('about/', views.About.as_view(), name="about"),
      path('index/', views.Index.as_view(), name="index"),
-     path('wayfarer/', views.WayfarerList.as_view(), name="wayfarer_list"),
-     path('wayfarer/new/', views.WayfarerCreate.as_view(), name="wayfarer_create"),
-     path('wayfarer/<int:pk>/', views.WayfarerDetail.as_view(), name="wayfarer_detail"),
-     path('wayfarer/<int:pk>/update',views.WayfarerUpdate.as_view(), name="wayfarer_update"),
-     path('wayfarer/<int:pk>/delete',views.WayfarerDelete.as_view(), name="wayfarer_delete"),
+     path('list/', views.WayfarerList.as_view(), name="spacefarer_list"),
+     path('spacefarer/new/', views.WayfarerCreate.as_view(), name="spacefarer_create"),
+     path('spacefarer/<int:pk>/', views.WayfarerDetail.as_view(), name="spacefarer_detail"),
+     path('spacefarer/<int:pk>/update',views.WayfarerUpdate.as_view(), name="spacefarer_update"),
+     path('spacefarer/<int:pk>/delete',views.WayfarerDelete.as_view(), name="spacefarer_delete"),
 
 
      path('accounts/signup/', views.Signup.as_view(), name="signup")
