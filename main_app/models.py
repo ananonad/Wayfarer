6b7f0d7abe60fa866b0fd13ca635bae5216a4304
annanadd <ananonadd@gmail.com>
2022-02-14 11:44:51 -0500

    user

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
 class Planet(models.Model):
 
@@ -8,9 +9,17 @@ class Planet(models.Model):
     bio = models.TextField(max_length=500)
     verified_planet = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
-    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.name
 
     class Meta:
         ordering = ['name']
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    favorite_color = models.CharField(max_length=50)
+
+#class User(AbstractUser):
--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
      path('', views.Home.as_view(), name="home"),
      path('about/', views.About.as_view(), name="about"),
      path('index/', views.Index.as_view(), name="index"),
-     path('wayfarer/', views.WayfarerList.as_view(), name="wayfarer_list")
+     path('wayfarer/', views.WayfarerList.as_view(), name="wayfarer_list"),
+     path('accounts/signup/', views.Signup.as_view(), name="signup")
 ]
