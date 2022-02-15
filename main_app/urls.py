2dbca4e06da236f4a61d51b471d2e2d502ed13aa
mikeyj913 <mikeyj913@hotmail.com>
2022-02-15 15:03:51 -0500

    added database

--- main_app/urls.py
+++ main_app/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
      path('<int:pk>/delete',views.Delete.as_view(), name="delete"),
      path('accounts/signup/', views.Signup.as_view(), name="signup"),
      path('profile/',  profile, name='userprofile'),
+
 ]
