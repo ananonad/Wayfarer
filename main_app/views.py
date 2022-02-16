9fc9a04d382e4a5a579452c2dee5fa6829d2041b
annanadd <ananonadd@gmail.com>
2022-02-16 00:03:51 -0500

    some updates for profile page

--- main_app/views.py
+++ main_app/views.py
@@ -1,33 +1,20 @@
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
-from platformdirs import user_cache_dir
+from django.contrib.auth.mixins import LoginRequiredMixin
+# from django.utils.decorators import method_decorator
+# from platformdirs import user_cache_dir
 from django.shortcuts import redirect
 from django.shortcuts import render
 from django.urls import reverse
-from .models import Planet, Profile
+from .models import Planet, Profile, User
+from django.contrib.auth.models import User
 from django.views import View 
-from django.views.generic.base import TemplateView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.views.generic import DetailView
+from django.views.generic import TemplateView, DetailView, CreateView, UpdateView, DeleteView
+from django import forms
 
-@method_decorator(login_required, name='dispatch')
 
-class ProfileView(View):
-    def get(self, request):
-        form = UserCreationForm()
-        context = {"form": form}
-        return render(request, "profile.html", context)
-    def post(self, request):
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect("home.html")
-        else:
-            context = {"form": form}
-            return render(request, "profile.html", context)
+
+ 
 
 class Landing(TemplateView):
     template_name = "landing.html"
@@ -39,7 +26,21 @@ class Index(TemplateView):
     template_name = "index.html"
 
 
+class Signup(View):
+    def get(self, request):
+        form = UserCreationForm()
+        context = {"form": form}
+        return render(request, "registration/signup.html", context)
 
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home.html")
+        else:
+            context = {"form": form}
+            return render(request, "registration/signup.html", context)
 
 class Home(TemplateView):
     template_name = "home.html"
@@ -79,8 +80,37 @@ class Create(CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super(Create, self).form_valid(form)
+
     def get_success_url(self):
         return reverse('detail', kwargs={'pk': self.object.pk})
+
+
+class ProfileCreate(CreateView):
+    model = Profile
+    fields = ['img', 'location', 'bio']
+    template_name = 'create_profile.html'
+
+    def get_success_url(self):
+        return reverse('detail_profile', kwargs={'pk': self.object.pk})
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+class ProfileDetail(DetailView):
+    model = Profile
+    template_name = "detail_profile.html"
+
+    def get_context_dat(self, **kwargs):
+        context = super().get_context_dat(**kwargs)
+        context['user'] = User.objects.filter(id = self.kwargs['pk'])
+        return context
+
+class ProfileUpdate(UpdateView):
+    model = Profile
+    fields = ['img', 'location', 'bio', 'username']
+    template_name = 'update_profile.html'
+
 
 class Detail(DetailView):
     model = Planet
@@ -99,17 +129,3 @@ class Delete(DeleteView):
 
 
 
-class Signup(View):
-    def get(self, request):
-        form = UserCreationForm()
-        context = {"form": form}
-        return render(request, "registration/signup.html", context)
-    def post(self, request):
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect("home.html")
-        else:
-            context = {"form": form}
-            return render(request, "registration/signup.html", context)
