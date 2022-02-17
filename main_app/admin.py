ce3c8031d33b89bc37036f7318e0f857e6a6d581
oclarkem23 <mikeyj913@hotmail.com>
2022-02-17 14:19:32 -0500

    finally got comments working

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,8 +1,8 @@
 from lib2to3.pgen2.token import COMMENT
 from django.contrib import admin
 from .models import Planet
-from .models import Profile, Comment
+from .models import User, Comment
 
 admin.site.register(Planet)
-admin.site.register(Profile)
+admin.site.register(User)
 admin.site.register(Comment)
